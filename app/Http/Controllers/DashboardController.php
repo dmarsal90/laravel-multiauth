62a769e6e51b43df9255aa1a6b9adfbf39ed1c95
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-11-16 17:35:54 -0500

    Add redirection for roles

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(){
+        if (Auth::user()->hasRole('user')){
+            return view('userdash');
+        }
+        if (Auth::user()->hasRole('writer')){
+            return view('writerdash');
+        }
+        if (Auth::user()->hasRole('admin')){
+            return view('dashboard');
+        }
+    }
 }
